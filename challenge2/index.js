c968b7385ad3a80c9cee064468516ef3e8a41c06
“Girish <girisheduru@gmail.com>
2021-03-31 06:48:01 +0100

    get metadata from GCP instance added

--- challenge2/index.js
+++ challenge2/index.js
@@ -1,8 +1,23 @@
 const express = require('express');
+const axios = require('axios');
+
 const app = express();
 
+const axiosInstance = axios.create({
+  baseURL: 'http://metadata.google.internal/',
+  timeout: 1000,
+  headers: {'Metadata-Flavor': 'Google'}
+});
+
 app.get('/', (req, res) => {
-  res.send('Hello from App Engine!');
+  let path = req.query.path || 'computeMetadata/v1/';
+
+  axiosInstance.get(path).then(response => {
+    console.log(response.status)
+    console.log(response.data);
+    res.send(response.data);
+  });
+
 });
 
 // Listen to the App Engine-specified port, or 8080 otherwise
